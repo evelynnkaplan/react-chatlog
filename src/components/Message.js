992fc68af42a857c080f6ea90b56415368dc12ff
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-11 16:27:13 -0700

    fixed bug with timestamp

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Timestamp from './Timestamp.js';
 
 const Message = (props) => {
-
   return (
     <section className="chat-entry local">
+      <p className="entry-name">{props.sender}</p>
       <section className="entry-bubble">
         <p className="entry-body">{props.body}</p>
+        <p className="entry-time"><Timestamp time={props.time} /></p>
       </section>
     </section>
   );
